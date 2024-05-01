cf14812bf137062a5d721d944224ee2cd1cef6ba
Blanca Pinot <bpinot@indexar.cl>
2024-05-01 13:23:58 -0400

    estilos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,1 @@
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
