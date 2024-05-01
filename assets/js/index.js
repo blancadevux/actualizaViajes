6475b646e3a268ecfd805b43d96a19e790496899
Blanca Pinot <bpinot@indexar.cl>
2024-05-01 16:24:17 -0400

    testimonio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,3 @@
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
